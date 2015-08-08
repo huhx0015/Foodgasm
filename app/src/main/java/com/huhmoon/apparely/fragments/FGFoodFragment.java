e6a19c2eb0fa3a361b60529e8923c4501cf47505
Michael Y. Huh <mhuh@yoonhuh.com>
2015-08-08 02:20:12 -0700

    * Added FGFoodFragment class.

---
+++ app/src/main/java/com/huhmoon/apparely/fragments/FGFoodFragment.java
@@ -0,0 +1,23 @@
+package com.huhmoon.apparely.fragments;
+
+import android.app.Activity;
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+/**
+ * Created by Michael Yoon Huh on 8/8/2015.
+ */
+
+public class FGFoodFragment extends Fragment {
+
+}
